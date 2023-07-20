d7b62312b716647c02e9a5f40e2f8ab9b3e5fec8
henrique <henriquevaz7@hotmail.com>
2023-07-20 23:55:20 +0100

    first attempts of sending IR NEC signals

--- app/src/main/java/com/example/toyotaremote/MainActivity.kt
+++ app/src/main/java/com/example/toyotaremote/MainActivity.kt
@@ -15,15 +15,46 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import com.example.toyotaremote.databinding.ActivityMainBinding
+import android.hardware.ConsumerIrManager
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var irManager: ConsumerIrManager? = null
+
+
+    private fun sendIrData() {
+        val frequency = 38000 // 38 kHz carrier frequency
+        val irData = intArrayOf(
+            // IR signal pattern (on/off durations in microseconds) for NEC protocol
+            9000, // Header: 9ms ON
+            4500, // Header: 4.5ms OFF
+            // Data bits (bitwise representation of hexadecimal value 0x37C84480)
+            560, 560, 560, 560, 560, 560, 560, 1690, // Address byte (8 bits)
+            560, 560, 560, 1690, 560, 560, 560, 560, // Address byte (inverted) (8 bits)
+            560, 560, 560, 1690, 560, 560, 560, 560, // Command byte (8 bits)
+            560, 560, 560, 1690, 560, 1690, 560, 1690, // Command byte (inverted) (8 bits)
+            // Add any required gaps or stop signals if needed
+        )
+
+        if (irManager?.hasIrEmitter() == true) {
+            irManager?.transmit(frequency, irData)
+            // The IR data is transmitted using the transmit method.
+        } else {
+            // Device does not have an IR transmitter.
+            // Handle the lack of IR transmitter based on your app's requirements.
+        }
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        irManager = getSystemService(CONSUMER_IR_SERVICE) as ConsumerIrManager
 
         val txtbtn = findViewById<Button>(R.id.txt)
         txtbtn.setOnClickListener(View.OnClickListener {
@@ -46,6 +77,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.ent.setOnClickListener(){
+            sendIrData()
             binding.textView.text = "ent selected"
         }
 
